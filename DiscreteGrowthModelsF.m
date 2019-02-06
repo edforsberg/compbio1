cf1508494a0eecf3e9087262c19c70aa8caf35b5
Erik Forsberg <erikforsberg95@hotmail.com>
2019-02-06 23:10:41 +0100

    code finally working

--- DiscreteGrowthModelsF.m
+++ DiscreteGrowthModelsF.m
@@ -1,6 +1,14 @@
 %subtask d) 
-timeSteps = 1000; 
-N0vec = [1,2,3,10];
+clc
+clf
+clear all 
+
+timeSteps = 100; 
+pertVec = [-10,-3,-2,-1,1,2,3,10];
+K = 1E3; 
+r = 0.1;
+KStar = K*r; 
+N0vec = KStar-pertVec; 
 
 N = []; 
 Nlin = []; 
@@ -12,19 +20,22 @@ N(1,i) = N0;
 Nlin(1,i) = N0; 
 for t = 1:timeSteps-1    
     N(t+1,i) = DescreteGrowth(N(t,i)); 
-    Nlin(t+1,i) = LinearStability(Nlin(t,i)); 
+    Nlin(t+1,i) = LinearStability(Nlin(t,i),KStar); 
 end
 end
 T = 1:timeSteps; 
 
+
 for p = 1:4
 subplot(2,2,p)
-loglog(T,N(:,p)); 
+semilogx(T,N(:,p)); 
 hold on 
-loglog(T,Nlin(:,p)); 
-legend({'exact','linearisation'},'Location','northwest')
+semilogx(T,Nlin(:,p)); 
+legend({'exact','linearisation'},'Location','southeast')
 xlabel('t') 
 ylabel('N(t)')
+xlim([min(T) max(T)]); 
+ylim([min(min([N(:,p) Nlin(:,p)]))-1 max(max([N(:,p) Nlin(:,p)]))+1]);
 title(sprintf('Population growth, N0 = %g',N0vec(p))); 
 end
 
